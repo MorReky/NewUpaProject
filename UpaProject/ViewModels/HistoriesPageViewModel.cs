28fb93b505f9efb5568f75a45996272c32da15ae
MorReky <mormemir@yandex.ru>
2021-02-03 08:21:13 +0000

    Обновлен интерфейс просмотра журналов. Добавлена возможность просмотра журналов входа пользователей

--- UpaProject/ViewModels/HistoriesPageViewModel.cs
+++ UpaProject/ViewModels/HistoriesPageViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UpaProject.DataFilesApp;
+using UpaProject.Models.DataFilesApp;
 using UpaProject.ViewModels.Base;
 
 namespace UpaProject.ViewModels
@@ -10,10 +12,27 @@ namespace UpaProject.ViewModels
     internal class HistoriesPageViewModel:ViewModel
     {
         #region Свойства
-
+        
+        public IEnumerable<HistoryLoginUsers> historyLoginUsers
+        {
+            //Внести правки. Пусть возвращает последние 20 элементов
+            get=> DBConnectHelper.DbObj.HistoryLoginUsers.ToList();
+        }
+        public IEnumerable<HistoryMTR> historyMTR
+        { 
+            get => DBConnectHelper.DbObj.HistoryMTR.ToList(); 
+        }
+        public IEnumerable<HistoryStorages> historyStorages
+        {
+            get => DBConnectHelper.DbObj.HistoryStorages.ToList();
+        }
         #endregion
         #region Комманды 
 
         #endregion
+        public HistoriesPageViewModel()
+        {
+
+        }
     }
 }
